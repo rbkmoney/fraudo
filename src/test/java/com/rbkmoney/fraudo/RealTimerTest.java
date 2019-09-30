efe12858174d1a1b649bdb06acff4a0c66be0800
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-09-30 14:01:34 +0300

    Fix timeouts for master

--- src/test/java/com/rbkmoney/fraudo/RealTimerTest.java
+++ src/test/java/com/rbkmoney/fraudo/RealTimerTest.java
@@ -3,21 +3,24 @@ package com.rbkmoney.fraudo;
 import com.rbkmoney.fraudo.constant.ResultStatus;
 import com.rbkmoney.fraudo.model.FraudModel;
 import com.rbkmoney.fraudo.model.ResultModel;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.stubbing.Answer;
 
 import java.io.InputStream;
 import java.util.concurrent.CountDownLatch;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 public class RealTimerTest extends AbstractFraudoTest {
+
+    public static final long TIME_CALL_AGGR_FUNC = 200L;
+    public static final long MILLISTIME_FAST_FUNC = 10L;
+    public static final long TIME_CALLING = 200L;
 
     @Before
     public void init() {
@@ -33,7 +36,7 @@ public class RealTimerTest extends AbstractFraudoTest {
         com.rbkmoney.fraudo.FraudoParser.ParseContext parseContext = getParseContext(resourceAsStream);
 
         FraudModel model = new FraudModel();
-        model.setAmount(10L);
+        model.setAmount(MILLISTIME_FAST_FUNC);
         model.setBin("444443");
 
         long start = System.currentTimeMillis();
@@ -41,7 +44,7 @@ public class RealTimerTest extends AbstractFraudoTest {
         long executionTime = System.currentTimeMillis() - start;
         assertEquals(ResultStatus.ACCEPT, result.getResultStatus());
         assertEquals(0, countDownLatch.getCount());
-        assertTrue(executionTime < 300);
+        assertTrue(executionTime < TIME_CALL_AGGR_FUNC + 1 + TIME_CALLING);
 
         System.out.println("executionTime=" + executionTime);
     }
@@ -55,7 +58,7 @@ public class RealTimerTest extends AbstractFraudoTest {
         com.rbkmoney.fraudo.FraudoParser.ParseContext parseContext = getParseContext(resourceAsStream);
 
         FraudModel model = new FraudModel();
-        model.setAmount(10L);
+        model.setAmount(MILLISTIME_FAST_FUNC);
         model.setBin("444443");
 
         long start = System.currentTimeMillis();
@@ -66,79 +69,79 @@ public class RealTimerTest extends AbstractFraudoTest {
         System.out.println("result=" + result.getRuleChecked());
 
         assertEquals(ResultStatus.ACCEPT, result.getResultStatus());
-        assertTrue(executionTime < 900);
+        assertTrue(executionTime < TIME_CALL_AGGR_FUNC * 4 + TIME_CALLING);
     }
 
     private void mockAggr(CountDownLatch countDownLatch) {
         when(countAggregator.count(any(), any(), any(), any()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     countDownLatch.countDown();
                     return 1;
                 });
         when(countAggregator.count(any(), any(), any()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     countDownLatch.countDown();
                     return 1;
                 });
         when(countAggregator.countSuccess(any(), any(), any(), any()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     countDownLatch.countDown();
                     return 1;
                 });
         when(countAggregator.countSuccess(any(), any(), any()))
                 .thenAnswer((Answer<Integer>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     countDownLatch.countDown();
                     return 1;
                 });
 
         when(sumAggregator.sum(any(), any(), any(), any()))
                 .thenAnswer((Answer<Double>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     return 10000.0;
                 });
         when(sumAggregator.sum(any(), any(), any()))
                 .thenAnswer((Answer<Double>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     return 10000.0;
                 });
         when(sumAggregator.sumSuccess(any(), any(), any(), any()))
                 .thenAnswer((Answer<Double>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     return 10000.0;
                 });
         when(sumAggregator.sumSuccess(any(), any(), any()))
                 .thenAnswer((Answer<Double>) invocationOnMock -> {
-                    Thread.sleep(200L);
+                    Thread.sleep(TIME_CALL_AGGR_FUNC);
                     return 10000.0;
                 });
 
         when(whiteListFinder.findInList(anyString(), anyString(), anyList(), anyList()))
                 .thenAnswer((Answer<Boolean>) invocationOnMock -> {
-                    Thread.sleep(10L);
+                    Thread.sleep(MILLISTIME_FAST_FUNC);
                     return false;
                 });
         when(greyListFinder.findInList(anyString(), anyString(), anyList(), anyList()))
                 .thenAnswer((Answer<Boolean>) invocationOnMock -> {
-                    Thread.sleep(10L);
+                    Thread.sleep(MILLISTIME_FAST_FUNC);
                     return false;
                 });
         when(greyListFinder.findInList(anyString(), anyString(), anyObject(), anyString()))
                 .thenAnswer((Answer<Boolean>) invocationOnMock -> {
-                    Thread.sleep(10L);
+                    Thread.sleep(MILLISTIME_FAST_FUNC);
                     return false;
                 });
         when(blackListFinder.findInList(anyString(), anyString(), anyObject(), anyString()))
                 .thenAnswer((Answer<Boolean>) invocationOnMock -> {
-                    Thread.sleep(10L);
+                    Thread.sleep(MILLISTIME_FAST_FUNC);
                     return false;
                 });
         when(blackListFinder.findInList(anyString(), anyString(), anyList(), anyList()))
                 .thenAnswer((Answer<Boolean>) invocationOnMock -> {
-                    Thread.sleep(10L);
+                    Thread.sleep(MILLISTIME_FAST_FUNC);
                     return false;
                 });
         when(countryResolver.resolveCountry(anyObject(), anyString()))
