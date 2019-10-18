c908da42c84cb4440f95a0d12ac1fc0c0d560a24
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-10-18 16:22:46 +0300

    Add field to model

--- src/main/java/com/rbkmoney/fraudo/model/FraudModel.java
+++ src/main/java/com/rbkmoney/fraudo/model/FraudModel.java
@@ -8,6 +8,7 @@ public class FraudModel {
     private String ip;
     private String email;
     private String bin;
+    private String pan;
     private String binCountryCode;
     private String cardToken;
     private String fingerprint;
