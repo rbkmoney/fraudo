9d4029b3a2722e51be0f6837927a8e27c8786da7
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-06-11 16:10:43 +0300

    Remove unused integer

--- src/main/java/com/rbkmoney/fraudo/p2p/visitor/impl/FirstFindP2PVisitorImpl.java
+++ src/main/java/com/rbkmoney/fraudo/p2p/visitor/impl/FirstFindP2PVisitorImpl.java
@@ -180,9 +180,7 @@ public class FirstFindP2PVisitorImpl<T extends BaseModel, U> extends FraudoP2PBa
 
     @Override
     public Double visitFloatExpression(FloatExpressionContext ctx) {
-        if (ctx.INTEGER() != null) {
-            return Double.valueOf(TextUtil.safeGetText(ctx.INTEGER()));
-        } else if (ctx.DECIMAL() != null) {
+        if (ctx.DECIMAL() != null) {
             return Double.valueOf(TextUtil.safeGetText(ctx.DECIMAL()));
         }
         return (Double) visitChildren(ctx);
